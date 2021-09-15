b3f03e4e1c2a536244a79db1aa4927c62fa23f44
soblab <soblab@WHIT208W10_00.it.farmingdale.edu>
2021-09-15 10:31:08 -0400

    Add the getStrLngth method to return ...

--- src/main/java/com/mycompany/week3_lec2/NewClass.java
+++ src/main/java/com/mycompany/week3_lec2/NewClass.java
@@ -3,13 +3,19 @@ package com.mycompany.week3_lec2;
 
 public class NewClass {
     
-    public int getSum(int a, int b){
-        return a+b;
+    // Lab3 Task: Create a method that takes a String and returns how many
+    // characters are there.
+    // Call this method and pass your name as an argument. 
+    
+    int getStrLngth(String name){
+        int l= name.length();
+        return l;
     }
+    
     public static void main(String[] args) {
         NewClass nc = new NewClass();
-        int x=10, y=100;
-        int z= nc.getSum(x,y);
-        System.out.println("The sum is "+ z);   
+        String str= "Moaath Alrajab";
+        int z= nc.getStrLngth(str);
+        System.out.println("The length is "+ z);   
     }
 }
